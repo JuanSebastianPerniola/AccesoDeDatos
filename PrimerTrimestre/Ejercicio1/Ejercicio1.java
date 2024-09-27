269f5d9c6ccdb290a30d59e46d3e817e540ff002
Juan Sebastian Perniola <jperniola@nigul.cide.es>
2024-09-27 13:43:42 +0200

    task ended

--- PrimerTrimestre/Ejercicio1/Ejercicio1.java
+++ PrimerTrimestre/Ejercicio1/Ejercicio1.java
@@ -2,13 +2,17 @@ package PrimerTrimestre.Ejercicio1;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
         try {
             // Crear objeto File con la ruta del archivo
-            FileInputStream readTxt = new FileInputStream(new File("C:\\Users\\JuanPrograma\\OneDrive\\Desktop\\AccesoDeDatos\\PrimerTrimestre\\Ejercicio1\\Archivo1.txt"));
+            File readTxt = new File(
+                    "C:\\Users\\JuanPrograma\\OneDrive\\Desktop\\AccesoDeDatos\\PrimerTrimestre\\Ejercicio1\\Archivo1.txt");
+            Scanner lectorDeTextoPorConsola = new Scanner(readTxt);
             // FileInputStream readJpg = new FileInputStream(new File("ejemplo1.jpg"));
+
             // Obtener la extensión del archivo
             String extension = readTxt.toString().substring(readTxt.toString().lastIndexOf(".") + 1);
 
@@ -16,17 +20,36 @@ public class Ejercicio1 {
             if (extension.equalsIgnoreCase("jpg")) {
                 try {
                     FileReaderBinary fileReaderBinary = new FileReaderBinary();
+                    // esta comentado por que estoy haciendo pruebas
                     // FileInputStream readJpg = new FileInputStream(file);
                     // fileReaderBinary.File(readJpg);
                 } catch (Exception e) {
                     System.out.println("Something went wrong " + e);
                 }
             } else if (extension.equalsIgnoreCase("txt")) {
-                // Si es un archivo de texto, llamamos al FileReader para leer texto
-                FileReader fileReader = new FileReader();
-                fileReader.readFile(readTxt);  // Pasamos la ruta del archivo
+                // leer archivos de texto
+                while (lectorDeTextoPorConsola.hasNextLine()) {
+                    String data = lectorDeTextoPorConsola.nextLine();
+                    System.out.println(data);
+                }
+                RevokePermission revokePermission = new RevokePermission();
+                revokePermission.revokePermissionBoolean(readTxt);
+
+                if (revokePermission.checkReaded == true) {
+                    try {
+                        while (lectorDeTextoPorConsola.hasNextLine()) {
+                            String data = lectorDeTextoPorConsola.nextLine();
+                            System.out.println(data);
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Algo a ido mal " + e);
+                    }
+                } else {
+                    System.out.println("EL archivo de texto no le han sido revocados los permisos");
+                }
+
             } else {
-                System.out.println("Tipo de archivo no soportado.");
+                System.out.println("Tipo de archivo no soportado");
             }
         } catch (Exception e) {
             System.out.println("Algo a ido mal " + e);
--- PrimerTrimestre/Ejercicio1/RevokePermission.java
+++ PrimerTrimestre/Ejercicio1/RevokePermission.java
@@ -1,5 +1,6 @@
 package PrimerTrimestre.Ejercicio1;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,17 +10,30 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class RevokePermission {
-    
+    //version 1 of revoking permission
+    //i have "think" in this way
+    // de other way it would be like this 
+    public boolean checkReaded;
+
     public void revokePermission(String filePath) {
         try {
             Path path = Paths.get(filePath);
             Set<PosixFilePermission> perms = new HashSet<>();
-            perms.add(PosixFilePermission.OWNER_READ);
+            perms.add(PosixFilePermission.OWNER_READ); 
             perms.add(PosixFilePermission.OWNER_WRITE);
 
             Files.setPosixFilePermissions(path, perms);
 
         } catch (IOException e) {
+            System.out.println("Error al revocar permisos: " + e.getMessage());
+        }
+    }
+    public void revokePermissionBoolean(File file) {
+        try {
+            //if have been read changed parameters
+            file.setReadable(false);
+            checkReaded = true;
+        } catch (Exception e) {
             System.out.println("Error al revocar permisos: " + e.getMessage());
         }
     }
